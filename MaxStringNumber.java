cc2527240cfbf7d391c4e0f753ca9068c0165b78
mahant95 <63593175+mahant95@users.noreply.github.com>
2022-10-11 20:18:52 +0530

    Added: Find Maximum String among given 3 numbers logic

---
+++ MaxStringNumber.java
@@ -0,0 +1,19 @@
+package com.Day13.mahant;
+
+public class MaxStringNumber {
+
+	public static String maxStringNumber(String num1, String num2, String num3)
+	{
+		String max = num1;
+		if(num1.compareTo(num2) > 0 && num1.compareTo(num3) > 0)
+			max = num1;
+		else if(num2.compareTo(num1) > 0 && num2.compareTo(num3) > 0)
+			max = num2;
+		else
+			max = num3;
+		return max;
+	}
+	public static void main(String[] args) {
+		System.out.println("The max Number Among 3 numbers is: "+maxStringNumber("Apple", "Peach", "Banana"));
+	}
+}
